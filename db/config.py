abea86ce3c73470699a5fd3f144f865a9aa37ce2
Oganes <ovikboss2016@gmail.com>
2025-04-11 20:53:09 +0300

    develop

--- db/config.py
+++ db/config.py
@@ -1,6 +1,6 @@
 class Settings:
     def __init__(self):
-        self.PASSWORD = "password"
+        self.PASSWORD = "myhardpassword"
         self.DBNAME = "telegram_db_1"
         self.PORT = 5432
         self.USER = "postgres"
